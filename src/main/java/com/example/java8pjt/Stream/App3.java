90c4301ec071f941973c866d41c299b0698b410f
방정환 <baju92@naver.com>
2021-05-23 23:47:39 +0900

    Stream API success commit

--- src/main/java/com/example/java8pjt/Stream/App3.java
+++ src/main/java/com/example/java8pjt/Stream/App3.java
@@ -1,7 +1,12 @@
 package com.example.java8pjt.Stream;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App3 {
     public static void main(String[] args) {
@@ -12,6 +17,36 @@ public class App3 {
         springClasses.add(new OnlineClass(4,"spring core", false));
         springClasses.add(new OnlineClass(5,"rest api development", false));
 
+        System.out.println("spring 으로 시작하는 수업");
+        springClasses.stream()
+                .filter(oc -> oc.getTitle().startsWith("spring"))
+                .forEach(oc -> System.out.println(oc.getId()));
+
+        System.out.println("==========================");
+        System.out.println("close 되지 않는 수업");
+        // 방법 1
+//        springClasses.stream()
+//                .filter(oc -> !oc.isClosed())
+//                .forEach(oc -> System.out.println(oc.getId()));
+
+        // 방법 2
+        // Predicate.not = ! 과 같은 역할 (java11에서만 가능)
+        springClasses.stream()
+                .filter(Predicate.not(OnlineClass::isClosed))
+                .forEach(oc -> System.out.println(oc.getId()));
+
+        System.out.println("==========================");
+        System.out.println("수업 이름만 모아서 스트림 만들기");
+        // 방법 1
+//        springClasses.stream()
+//                .map(oc -> oc.getTitle())
+//                .forEach(s -> System.out.println(s));
+
+        // 방법 2
+        springClasses.stream()
+                .map(OnlineClass::getTitle)
+                .forEach(System.out::println);
+
         List<OnlineClass> javaClasses = new ArrayList<>();
         javaClasses.add(new OnlineClass(6,"The Java, Test", true));
         javaClasses.add(new OnlineClass(7,"The Java, Code manipulation", true));
@@ -21,23 +56,41 @@ public class App3 {
         jeongHwanEvents.add(springClasses);
         jeongHwanEvents.add(javaClasses);
 
-        System.out.println("spring 으로 시작하는 수업");
-        // TODO
-
-        System.out.println("close 되지 않는 수업");
-        // TODO
-
-        System.out.println("수업 이름만 모아서 스트림 만들기");
-        // TODO
-
+        System.out.println("==========================");
         System.out.println("두 수업 목록에 들어있는 모든 수업 아이디 출력");
-        // TODO
+        jeongHwanEvents.stream()
+//                .flatMap(list -> list.stream())
+                .flatMap(Collection::stream)
+//                .forEach(oc -> System.out.println(oc.getId()));
+                .map(OnlineClass::getId)
+                .forEach(System.out::println);
 
+        System.out.println("==========================");
         System.out.println("10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만");
-        // TODO
+        // 중계형 오퍼레이터 이기 때문에 아무동작도 하지 않는다. / 반환값이 Stream이다.
+        Stream.iterate(10,i -> i+1);
 
+        // 무한대로 출력된다.
+//        Stream.iterate(10,i -> i+1)
+//                .forEach(System.out::println);
+        Stream.iterate(10,i -> i+1)
+                .skip(10)
+                .limit(10)
+                .forEach(System.out::println); //20 ~ 29
+
+        System.out.println("==========================");
         System.out.println("자바 수업 중에 Test가 들어있는 수업이 있는지 확인");
-        // TODO
+        //anyMatch() 를 사용
+        boolean test = javaClasses.stream()
+                .anyMatch(jc -> jc.getTitle().contains("Test"));
+        System.out.println(test);
 
+        System.out.println("==========================");
+        System.out.println("스프링 수업 중에 제목이 spring이 들어간 것만 모아서 List 로 만들기");
+        List<String> spring = springClasses.stream()
+                .filter(oc -> oc.getTitle().contains("spring"))
+                .map(OnlineClass::getTitle)
+                .collect(Collectors.toList());
+        spring.forEach(System.out::println);
     }
 }
